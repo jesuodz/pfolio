11668437e8f17a26bbade8cd58a9b99d6b3dec29
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-02-11 01:04:31 -0400

    Add several changes
    - Change colors
    - Remove outline from links, now they are prettier :)
    - Change background and colors of text during selection

--- src/js/scroll-navbar.js
+++ src/js/scroll-navbar.js
@@ -2,8 +2,8 @@ const navbar = document.getElementById('header');
 
 window.onscroll = function() {
   "use strict";
-  if (document.documentElement.scrollTop >= window.innerHeight ||
-      document.body.scrollTop >= window.innerHeight) {
+  if (document.documentElement.scrollTop >= window.innerHeight * 0.75  ||
+      document.body.scrollTop >= window.innerHeight * 0.75 ) {
     navbar.classList.add('header--scroll');
   } else {
     navbar.classList.remove('header--scroll');
